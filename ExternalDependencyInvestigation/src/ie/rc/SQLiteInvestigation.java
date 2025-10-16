540297121c1f30ab95a7cd7b94596875b0e9082e
Aidan Killeen <aidan.killeen@gmail.com>
2025-10-16 10:53:11 +0100

    removed unused db

--- ExternalDependencyInvestigation/src/ie/rc/SQLiteInvestigation.java
+++ ExternalDependencyInvestigation/src/ie/rc/SQLiteInvestigation.java
@@ -24,32 +24,26 @@ public class SQLiteInvestigation {
 		
 		String url = "jdbc:sqlite:C:\\work\\training\\java\\JavaFoundationOctober2025\\users.db";
 		
+		// DriverManager.getConnection(url);
+		
+		
 		
 		try {
 			Connection conn = DriverManager.getConnection(url);
-			
 			String sql = "SELECT * FROM users";
-			
 			Statement stmt = conn.createStatement();
-			
 			ResultSet rs = stmt.executeQuery(sql);
 			
 			while (rs.next() ) {
-				 
+				boolean active = rs.getBoolean(4);
+				
 				System.out.printf("%d %s %s\n", 
 						rs.getInt(1), rs.getString(2), rs.getString(3));
 			}
-			
 			conn.close();
-			
 		} catch (SQLException e) {
 			
 			e.printStackTrace();
 		}
-		
-		
-		
-
 	}
-
 }
